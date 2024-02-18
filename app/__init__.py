74fc6e1c7c356c4050882552ad2de1de3eb9eabe
PKForm <prakrid14432@gmail.com>
2024-02-19 02:30:33 +0700

    Config Web App Server & Deploy Web App

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the Flask app with debug mode disabled
+    app.run(host='0.0.0.0', port=80)
