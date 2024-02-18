f601ec3e3cb590149ebdd79afa0000a341471857
PKForm <prakrid14432@gmail.com>
2024-02-18 16:27:11 +0700

    Config Wep App Server

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Change directory to Django project root
-cd ~/app  # Assuming the Django project is in the 'app' directory in the home directory
-
 # Activate virtual environment (if applicable)
 # source /path/to/venv/bin/activate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python3 manage.py migrate
 
 # Restart web server (if necessary)
 # sudo systemctl restart gunicorn
