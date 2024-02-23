868f39f050aba3d6d99f54980aac8363a5199da9
PKForm <prakrid14432@gmail.com>
2024-02-24 03:52:33 +0700

    Initial Wep App

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/reset-password/')
+def reset_password():
+    return render_template('reset-password.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
