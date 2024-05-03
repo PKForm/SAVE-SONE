447d01f6bc6eab154fddd2eda74533c538c8e1c6
PKForm <prakrid14432@gmail.com>
2024-05-04 05:33:53 +0700

    Update Frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route('/')
 def index():
