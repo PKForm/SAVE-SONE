77282ebf2a20f6d94fb742c4a475889cdaf8af60
PKForm <prakrid14432@gmail.com>
2024-05-04 04:02:50 +0700

    Update Frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'save-sone-project'
 
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/signup/')
+@app.route('/sign_up')
 def signup():
-    return render_template('signup.html')
+    return render_template('sign_up.html')
 
-@app.route('/reset-password/')
+@app.route('/reset_password')
 def reset_password():
-    return render_template('reset-password.html')
+    return render_template('reset_password.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
